a8319592274e0ed15fed95718c3e9148eb52afa0
Bruno Damasio <133672309+iwtbeiwbgd@users.noreply.github.com>
2024-10-01 23:04:37 +0000

    01-10-24

--- index.js
+++ index.js
@@ -13,6 +13,28 @@ idade = idade +1;
 
 console.log (`oi eu sou ${nomeCompleto} meu ano de nascimento é ${anoNascimento} e estamos no ano ${anoAtual} e minha idade é ${idade}`);
 
+const loginCerto = "Bruno";
+const senhaCerta = "9999";
+console.log(" ");
+console.log("AGENCIAS DE VIAGENS");
+console.log(" ");
+console.log("Faça seu login");
+
+var login = entrada("Digite seu login: ");
+var senha = entrada("digite sua senha: ");
+
+while(login != loginCerto){
+  console.log ("login incorreto");
+  login = entrada("digite seu login novamente: ");
+}
+
+while(senha != senhaCerta){
+  console.log ("senha incorreta");
+  senha = entrada("digite sua senha novamente: ");
+}
+
+
+
 const listaDeViagens = new Array(
 'Japão', 
 'Londres',
@@ -28,8 +50,8 @@ console.log(listaDeViagens);
 
 listaDeViagens.splice(3,1);
 
-var nomeComprador = entrada("Qual seu nome?");
-var idadeComprador = entrada("Qual sua idade?");
+var nomeComprador = entrada("Qual seu nome? ");
+var idadeComprador = entrada("Qual sua idade? ");
 
 if (idadeComprador<18){
   
